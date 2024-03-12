7454515cf3cb8d677354fdd26f72622795c33116
Gektor <bheiko@salerow.software>
2024-03-12 12:34:05 +0200

    fix change language

--- src/features/account/index.tsx
+++ src/features/account/index.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, View } from "react-native"
-import { useLayoutEffect, useRef } from "react"
+import { useLayoutEffect } from "react"
 import { usePathname, useRouter } from "expo-router"
 import { useTranslation } from "react-i18next"
 
@@ -21,20 +21,13 @@ import { PersonalInformation } from "./_components/personalInformation"
 import { RieltorInformation } from "./_components/rieltorInformation"
 import { YourAccount } from "./_components/yourAccount"
 import { style } from "./_style"
-import BottomSheet from "@gorhom/bottom-sheet"
-import CustomBottomSheet from "@/shared/components/bottomSheet/bottomSheet.component"
-import { useDispatch } from "react-redux"
-import { TLanguage } from "@/types"
-import { mainApi } from "@/store/services/mainApi"
 
 export const Account = () => {
   const router = useRouter()
   const pathName = usePathname()
-  const dispatch = useDispatch()
-  const { logoutUser, setLanguage } = useActions()
-  const { t, i18n } = useTranslation("account")
+  const { logoutUser } = useActions()
+  const { t } = useTranslation("account")
 
-  const bottomSheetRef = useRef<BottomSheet>(null)
   const userData = useAppSelector((state) => state.user_data)
   const { isAuthenticated } = useAppSelector((state) => state.bober_auth)
 
@@ -54,22 +47,6 @@ export const Account = () => {
     router.replace("/(tabs)/")
   }
 
-  const handleClosePress = () => bottomSheetRef.current?.close()
-  const handleOpenPress = () => bottomSheetRef.current?.expand()
-
-  const onSetLanguage = (item: TLanguage): void => {
-    setLanguage(item)
-    i18n.changeLanguage(item)
-    handleClosePress()
-    dispatch(
-      mainApi.util.invalidateTags([
-        "UserPublicBuildings",
-        "UserPublicBuilers",
-        "UserBuildings"
-      ])
-    )
-  }
-
   useLayoutEffect(() => {
     if (!isAuthenticated) {
       return router.replace("/(public)/(auth)/signin")
@@ -77,64 +54,46 @@ export const Account = () => {
   }, [isAuthenticated])
 
   return (
-    <>
-      <ScrollView
-        overScrollMode="never"
-        nestedScrollEnabled={false}
-        showsVerticalScrollIndicator={false}
-        style={{ backgroundColor: colors.white }}
-      >
-        {/* <CustomBottomSheet ref={bottomSheetRef} /> */}
-
-        <View style={style.mainContainer}>
-          <YourAccount t={t} investments={userData.totalBalance} />
-          {userData.isRealtor && (
-            <RieltorInformation
-              t={t}
-              inviteLink={userData.inviteLink}
-              realtorPercent={userData.realtorPercent}
-            />
-          )}
-          <AccrualAccount
+    <ScrollView
+      overScrollMode="never"
+      nestedScrollEnabled={false}
+      showsVerticalScrollIndicator={false}
+      style={{ backgroundColor: colors.white }}
+    >
+      <View style={style.mainContainer}>
+        <YourAccount t={t} investments={userData.totalBalance} />
+        {userData.isRealtor && (
+          <RieltorInformation
             t={t}
-            title={t("Нарахування")}
-            isLoading={isUserAccrualLoading}
-            accrualData={userAccrualsData}
+            inviteLink={userData.inviteLink}
+            realtorPercent={userData.realtorPercent}
           />
-          {!userData.isRealtor && (
-            <>
-              <InvestmentAccount
-                t={t}
-                title={t("Інвестиції")}
-                isLoading={isUserInvestmentsLoading}
-                investmentsData={userInvestmentsData}
-              />
-              <MyProjects
-                t={t}
-                projectsData={userBuildingsData}
-                isLoading={isUserBuildingsLoading}
-              />
-            </>
-          )}
-          <PersonalInformation t={t} data={userData} openLanguage={handleOpenPress} />
-          <Button onPress={handleLogout} title={t("Вихід")} variant="secondary" />
-          <OrganizationInfo />
-        </View>
-      </ScrollView>
-      <CustomBottomSheet ref={bottomSheetRef}>
-        <>
-          <Button
-            onPress={() => onSetLanguage("uk-UA")}
-            title={t("Ukranian")}
-            variant="secondary"
-          />
-          <Button
-            onPress={() => onSetLanguage("en-US")}
-            title={t("English")}
-            variant="secondary"
-          />
-        </>
-      </CustomBottomSheet>
-    </>
+        )}
+        <AccrualAccount
+          t={t}
+          title={t("Нарахування")}
+          isLoading={isUserAccrualLoading}
+          accrualData={userAccrualsData}
+        />
+        {!userData.isRealtor && (
+          <>
+            <InvestmentAccount
+              t={t}
+              title={t("Інвестиції")}
+              isLoading={isUserInvestmentsLoading}
+              investmentsData={userInvestmentsData}
+            />
+            <MyProjects
+              t={t}
+              projectsData={userBuildingsData}
+              isLoading={isUserBuildingsLoading}
+            />
+          </>
+        )}
+        <PersonalInformation t={t} data={userData} />
+        <Button onPress={handleLogout} title={t("Вихід")} variant="secondary" />
+        <OrganizationInfo />
+      </View>
+    </ScrollView>
   )
 }
--- src/shared/components/bottomSheet/bottomSheet.component.tsx
+++ src/shared/components/bottomSheet/bottomSheet.component.tsx
@@ -1,11 +1,5 @@
-import { forwardRef, useCallback, useMemo, useRef } from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import BottomSheet, {
-  BottomSheetBackdrop,
-  BottomSheetModal,
-  BottomSheetView
-} from "@gorhom/bottom-sheet"
+import { forwardRef, useCallback, useMemo } from "react"
+import BottomSheet, { BottomSheetBackdrop, BottomSheetView } from "@gorhom/bottom-sheet"
 import { colors } from "@/utils/constants/colors"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
@@ -17,7 +11,7 @@ interface CustomBottomSheetProps {
 
 const CustomBottomSheet = forwardRef<Ref, CustomBottomSheetProps>((props, ref) => {
   const initialSnapPoints = useMemo(() => ["10%"], [])
-  const insets = useSafeAreaInsets();
+  const insets = useSafeAreaInsets()
 
   const renderBackdrop = useCallback(
     (props: any) => (
@@ -47,20 +41,3 @@ const CustomBottomSheet = forwardRef<Ref, CustomBottomSheetProps>((props, ref) =
 })
 
 export default CustomBottomSheet
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  contentContainer: {
-    flex: 1,
-    alignItems: "center"
-  },
-  containerHeadline: {
-    fontSize: 24,
-    fontWeight: "600",
-    padding: 20
-  }
-})
